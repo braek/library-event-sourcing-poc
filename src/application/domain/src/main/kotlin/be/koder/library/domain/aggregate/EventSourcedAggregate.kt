c6afdcb937916e7c3dac26b802b1b8ffba587e6d
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-03 22:40:48 +0200

    WIP (work in progress)

---
+++ src/application/domain/src/main/kotlin/be/koder/library/domain/aggregate/EventSourcedAggregate.kt
@@ -0,0 +1,24 @@
+package be.koder.library.domain.aggregate
+
+import be.koder.library.domain.event.Event
+import be.koder.library.domain.event.EventStream
+
+abstract class EventSourcedAggregate(private val statusQuo: EventStream = EventStream(emptyList())) : Aggregate {
+
+    private val mutations: ArrayList<Event> = ArrayList()
+
+    protected fun apply(event: Event) {
+        dispatch(event)
+        mutations.add(event)
+    }
+
+    fun isMutated(): Boolean {
+        return mutations.isNotEmpty()
+    }
+
+    fun getMutations(): EventStream {
+        return EventStream(mutations)
+    }
+
+    protected abstract fun dispatch(event: Event);
+}
