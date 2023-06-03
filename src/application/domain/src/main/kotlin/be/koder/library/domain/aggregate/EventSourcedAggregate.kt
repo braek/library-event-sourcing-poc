104b5b09087358bb53bfff02f43b70a636b2caa1
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-03 23:02:59 +0200

    WIP (work in progress)

--- src/application/domain/src/main/kotlin/be/koder/library/domain/aggregate/EventSourcedAggregate.kt
+++ src/application/domain/src/main/kotlin/be/koder/library/domain/aggregate/EventSourcedAggregate.kt
@@ -3,11 +3,17 @@ package be.koder.library.domain.aggregate
 import be.koder.library.domain.event.Event
 import be.koder.library.domain.event.EventStream
 
-abstract class EventSourcedAggregate private constructor(
-    private val statusQuo: EventStream = EventStream(emptyList())
-) : Aggregate {
+abstract class EventSourcedAggregate : Aggregate {
 
+    private var statusQuo: EventStream = EventStream(emptyList())
     private val mutations: ArrayList<Event> = ArrayList()
+
+    constructor(stream: EventStream) {
+        this.statusQuo = stream
+        this.statusQuo.events.forEach {
+            dispatch(it)
+        }
+    }
 
     protected fun apply(event: Event) {
         dispatch(event)
