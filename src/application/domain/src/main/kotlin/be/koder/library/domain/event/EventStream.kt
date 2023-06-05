e56da1706766ec53390faa8a7071fb8ddb4c0099
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-05 20:30:33 +0200

    WIP (work in progress)

--- src/application/domain/src/main/kotlin/be/koder/library/domain/event/EventStream.kt
+++ src/application/domain/src/main/kotlin/be/koder/library/domain/event/EventStream.kt
@@ -1,8 +1,13 @@
 package be.koder.library.domain.event
 
 data class EventStream(private val events: List<Event>) : Iterable<Event> {
+
     fun isEmpty(): Boolean {
         return events.isEmpty()
+    }
+
+    override fun iterator(): Iterator<Event> {
+        return events.iterator();
     }
 
     companion object {
@@ -10,9 +15,5 @@ data class EventStream(private val events: List<Event>) : Iterable<Event> {
         fun empty(): EventStream {
             return EventStream(emptyList())
         }
-    }
-
-    override fun iterator(): Iterator<Event> {
-        return events.iterator();
     }
 }
