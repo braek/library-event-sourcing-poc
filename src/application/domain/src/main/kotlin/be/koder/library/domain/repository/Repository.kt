8241e02e4f574b1d7e5d32b12ba39ba5a1ea5b61
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-05 14:32:58 +0200

    WIP (work in progress)

---
+++ src/application/domain/src/main/kotlin/be/koder/library/domain/repository/Repository.kt
@@ -0,0 +1,11 @@
+package be.koder.library.domain.repository
+
+import be.koder.library.domain.aggregate.Aggregate
+import be.koder.library.vocabulary.domain.AggregateId
+import java.util.*
+
+interface Repository<ID : AggregateId, AGGREGATE : Aggregate> {
+    fun get(id: ID): Optional<AGGREGATE>
+
+    fun save(aggregate: AGGREGATE)
+}
