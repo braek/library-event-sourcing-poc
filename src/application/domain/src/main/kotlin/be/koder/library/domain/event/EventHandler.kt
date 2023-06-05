b0e7b6ebc4f33606eb5635e3de3dc07ba6c517c2
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-05 14:49:28 +0200

    WIP (work in progress)

---
+++ src/application/domain/src/main/kotlin/be/koder/library/domain/event/EventHandler.kt
@@ -0,0 +1,5 @@
+package be.koder.library.domain.event
+
+interface EventHandler {
+    fun handle(event: Event)
+}
---
+++ src/test-doubles/src/main/kotlin/be/koder/library/test/MockEventPublisher.kt
@@ -0,0 +1,26 @@
+package be.koder.library.test
+
+import be.koder.library.domain.event.Event
+import be.koder.library.domain.event.EventHandler
+import be.koder.library.domain.event.EventPublisher
+
+class MockEventPublisher : EventPublisher {
+
+    private val handlers = ArrayList<EventHandler>()
+    private val events = ArrayList<Event>()
+
+    override fun publish(event: Event) {
+        events.add(event)
+        handlers.forEach {
+            it.handle(event)
+        }
+    }
+
+    fun subscribe(handler: EventHandler) {
+        handlers.add(handler)
+    }
+
+    fun getEvents(): List<Event> {
+        return events.toList()
+    }
+}
