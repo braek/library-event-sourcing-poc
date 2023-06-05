799dd1a68f87bd15408a21d4d2b67169f438c025
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-05 22:56:59 +0200

    WIP (work in progress)

--- src/test-doubles/src/main/kotlin/be/koder/library/test/MockEventStore.kt
+++ src/test-doubles/src/main/kotlin/be/koder/library/test/MockEventStore.kt
@@ -3,14 +3,18 @@ package be.koder.library.test
 import be.koder.library.domain.event.Event
 import be.koder.library.domain.event.EventStore
 import be.koder.library.domain.event.EventStream
+import be.koder.library.vocabulary.domain.AggregateId
+import java.util.stream.Collectors
 
 class MockEventStore : EventStore {
 
     private val events: ArrayList<Event> = ArrayList()
 
     override fun append(eventStream: EventStream) {
-        eventStream.forEach {
-            events.add(it)
-        }
+        eventStream.forEach { events.add(it) }
+    }
+
+    fun query(aggregateId: AggregateId): EventStream {
+        return EventStream(events.stream().filter { it.tags().contains(aggregateId) }.collect(Collectors.toList()))
     }
 }
