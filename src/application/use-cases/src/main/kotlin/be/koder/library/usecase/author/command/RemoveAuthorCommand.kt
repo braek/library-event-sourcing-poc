4c496472046fe3945356c0c3b3b1da0b7aba781c
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-07 09:06:39 +0200

    WIP (work in progress)

--- src/application/use-cases/src/main/kotlin/be/koder/library/usecase/author/command/RemoveAuthorCommand.kt
+++ src/application/use-cases/src/main/kotlin/be/koder/library/usecase/author/command/RemoveAuthorCommand.kt
@@ -3,4 +3,6 @@ package be.koder.library.usecase.author.command
 import be.koder.library.domain.cqrs.Command
 import be.koder.library.vocabulary.author.AuthorId
 
-data class RemoveAuthorCommand(val authorId: AuthorId) : Command
+data class RemoveAuthorCommand(
+    val authorId: AuthorId
+) : Command
