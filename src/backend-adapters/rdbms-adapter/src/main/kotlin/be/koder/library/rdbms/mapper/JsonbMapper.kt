dbfe7dc508f12e7a10ea23fb76a1e6f4702158af
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-12 22:36:34 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/mapper/JsonbMapper.kt
+++ src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/mapper/JsonbMapper.kt
@@ -1,6 +1,10 @@
 package be.koder.library.rdbms.mapper
 
 import be.koder.library.domain.event.Event
+import be.koder.library.vocabulary.domain.AggregateId
+import be.koder.library.vocabulary.event.EventId
+import be.koder.library.vocabulary.time.Timestamp
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.jooq.JSONB
 import java.io.IOException
@@ -19,5 +23,17 @@ object JsonbMapper {
         } catch (e: IOException) {
             throw RuntimeException(e)
         }
+    }
+
+    abstract class EventMixin {
+
+        @JsonIgnore
+        abstract fun id(): EventId
+
+        @JsonIgnore
+        abstract fun occurredOn(): Timestamp
+
+        @JsonIgnore
+        abstract fun tags(): Set<AggregateId>
     }
 }
