6d63b61c3c6a7bc0f7619cefada062a46a3fe9e3
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-14 15:16:16 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/TagMapper.kt
+++ src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/TagMapper.kt
@@ -9,9 +9,9 @@ object TagMapper {
     fun map(str: String): AggregateId {
         val regex = Pattern.compile("^\\w+#\\w+$")
         if (regex.matcher(str).matches()) {
-            val chunks = str.split("#")
-            val type = chunks[0]
-            val value = chunks[1]
+            val parts = str.split("#")
+            val type = parts[0]
+            val value = parts[1]
             if (AuthorId::class.java.simpleName.equals(type)) {
                 return AuthorId.fromString(value)
             }
