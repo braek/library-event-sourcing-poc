c5588d60800960c8c379674feb433065c8e081bf
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-15 21:55:22 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/stored/StoredEvent.kt
+++ src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/stored/StoredEvent.kt
@@ -3,7 +3,10 @@ package be.koder.library.rdbms.event.stored
 import be.koder.library.domain.event.Event
 
 interface StoredEvent {
+
     val id: String
+
     val occurredOn: String
+
     fun toEvent(): Event
 }
