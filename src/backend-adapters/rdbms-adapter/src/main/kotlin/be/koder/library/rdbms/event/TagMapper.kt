fe31cdbbe27abc63f202c4f99b87be4834a5c8fb
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-16 22:34:11 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/TagMapper.kt
+++ src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/TagMapper.kt
@@ -2,23 +2,8 @@ package be.koder.library.rdbms.event
 
 import be.koder.library.vocabulary.author.AuthorId
 import be.koder.library.vocabulary.domain.AggregateId
-import java.util.regex.Pattern
 
 object TagMapper {
-
-    private val regex = Pattern.compile("^\\w+#\\w+$")
-
-    fun map(str: String): AggregateId {
-        if (regex.matcher(str).matches()) {
-            val parts = str.split("#")
-            val type = parts[0]
-            val value = parts[1]
-            if (Tag.AUTHOR.name.lowercase() == type) {
-                return AuthorId.fromString(value)
-            }
-        }
-        throw IllegalArgumentException(String.format("Cannot map String (%s) to AggregateId", str))
-    }
 
     fun map(aggregateId: AggregateId): String {
         if (aggregateId is AuthorId) {
