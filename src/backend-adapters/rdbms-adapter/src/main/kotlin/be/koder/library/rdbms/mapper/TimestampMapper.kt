21e4bcf5851bceea5ae8401f4c8f224cd566c1f4
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-17 09:48:16 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/mapper/TimestampMapper.kt
+++ src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/mapper/TimestampMapper.kt
@@ -5,15 +5,18 @@ import java.util.regex.Pattern
 
 object TimestampMapper {
 
+    private val regexSeconds = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z$")
+    private val regexMicroseconds = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z$")
+
     // Purpose: make sure that the Timestamp is written as String with precision of milliseconds
     fun map(timestamp: Timestamp): String {
         val str = timestamp.getValue().toString()
         // Precision = seconds
-        if (Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z$").matcher(str).matches()) {
+        if (regexSeconds.matcher(str).matches()) {
             return "${str.substring(0, 19)}.000000Z"
         }
         // Precision = microseconds
-        if (Pattern.compile("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}Z$").matcher(str).matches()) {
+        if (regexMicroseconds.matcher(str).matches()) {
             return "${str.substring(0, 23)}000Z"
         }
         return str
