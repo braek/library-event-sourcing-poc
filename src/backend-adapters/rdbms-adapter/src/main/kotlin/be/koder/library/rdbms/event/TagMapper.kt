ca8c3fc79346d996f8975a276da27ada0d1eaccc
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-24 00:40:53 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/TagMapper.kt
+++ src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/TagMapper.kt
@@ -1,6 +1,7 @@
 package be.koder.library.rdbms.event
 
 import be.koder.library.vocabulary.author.AuthorId
+import be.koder.library.vocabulary.book.BookId
 import be.koder.library.vocabulary.domain.AggregateId
 
 object TagMapper {
@@ -9,10 +10,14 @@ object TagMapper {
         if (aggregateId is AuthorId) {
             return Tag.AUTHOR.name.lowercase() + "#" + aggregateId.toString()
         }
+        if (aggregateId is BookId) {
+            return Tag.BOOK.name.lowercase() + "#" + aggregateId.toString()
+        }
         throw IllegalArgumentException(String.format("Cannot map AggregateId (%s) to String", aggregateId.toString()))
     }
 
     enum class Tag {
-        AUTHOR
+        AUTHOR,
+        BOOK
     }
 }
