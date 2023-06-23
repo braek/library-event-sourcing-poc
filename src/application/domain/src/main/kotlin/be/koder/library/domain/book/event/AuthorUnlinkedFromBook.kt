25976c8d92ca52e1d56116979d8d5416573b1206
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-24 00:40:02 +0200

    WIP (work in progress)

---
+++ src/application/domain/src/main/kotlin/be/koder/library/domain/book/event/AuthorUnlinkedFromBook.kt
@@ -0,0 +1,25 @@
+package be.koder.library.domain.book.event
+
+import be.koder.library.domain.event.Event
+import be.koder.library.vocabulary.author.AuthorId
+import be.koder.library.vocabulary.book.BookId
+import be.koder.library.vocabulary.domain.AggregateId
+import be.koder.library.vocabulary.event.EventId
+import be.koder.library.vocabulary.time.Timestamp
+
+data class AuthorUnlinkedFromBook internal constructor(
+    override val id: EventId,
+    override val occurredOn: Timestamp,
+    override val tags: Set<AggregateId>,
+    val author: AuthorId,
+    val book: BookId
+) : Event {
+
+    constructor(author: AuthorId, book: BookId) : this(
+        EventId.createNew(),
+        Timestamp.now(),
+        setOf(author, book),
+        author,
+        book
+    )
+}
