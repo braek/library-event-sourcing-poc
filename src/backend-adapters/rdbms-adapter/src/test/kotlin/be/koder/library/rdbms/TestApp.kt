0f2d03f05c5499e89bbd00913114b5c1b72411ed
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-23 22:57:04 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/test/kotlin/be/koder/library/rdbms/TestApp.kt
+++ src/backend-adapters/rdbms-adapter/src/test/kotlin/be/koder/library/rdbms/TestApp.kt
@@ -1,14 +1,15 @@
 package be.koder.library.rdbms
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.test.context.ContextConfiguration
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [DataSourceAutoConfiguration::class]
+)
 @ContextConfiguration(
-    classes = [
-        RdbmsConfig::class
-    ]
+    classes = [RdbmsConfig::class]
 )
 open class TestApp {
     fun main(args: Array<String>) {
