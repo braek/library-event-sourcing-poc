f88a5fb5cb2b0df4b471a5832f31e95316df0c2e
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-06-25 00:16:25 +0200

    WIP (work in progress)

--- src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/RdbmsEventStore.kt
+++ src/backend-adapters/rdbms-adapter/src/main/kotlin/be/koder/library/rdbms/event/RdbmsEventStore.kt
@@ -3,10 +3,13 @@ package be.koder.library.rdbms.event
 import be.koder.library.domain.aggregate.EventSourcedAggregate
 import be.koder.library.domain.event.EventStore
 import be.koder.library.domain.event.EventStream
+import be.koder.library.rdbms.event.json.EventJsonMapper
 import be.koder.library.rdbms.tables.records.EventStoreRecord
+import be.koder.library.rdbms.tables.references.EVENT_STORE
 import be.koder.library.vocabulary.domain.AggregateId
 import be.koder.library.vocabulary.event.EventId
 import org.jooq.DSLContext
+import org.jooq.impl.DSL.orderBy
 import org.springframework.transaction.annotation.Transactional
 import java.lang.RuntimeException
 
@@ -32,22 +35,23 @@ open class RdbmsEventStore(private val dsl: DSLContext) : EventStore {
     }
 
     override fun query(aggregateId: AggregateId): EventStream {
-//        val tags = query.tags.stream()
-//            .map { TagMapper.map(it) }
-//            .collect(Collectors.toSet())
-//        return EventStream(dsl.selectFrom(EVENT_STORE)
-//            .where(EVENT_STORE.TAGS.contains(tags.toTypedArray()))
-//            .and(EVENT_STORE.TYPE.`in`(query.types))
-//            .orderBy(EVENT_STORE.SEQUENCE_ID.asc())
-//            .fetch()
-//            .map { EventJsonMapper.toEvent(it.payload!!, it.type!!) }
-//            .toList()
-//        )
-        return EventStream.empty()
+        return EventStream(dsl.selectFrom(EVENT_STORE)
+            .where(EVENT_STORE.TAGS.contains(arrayOf(TagMapper.map(aggregateId))))
+            .orderBy(EVENT_STORE.SEQUENCE_ID.asc())
+            .fetch()
+            .map { EventJsonMapper.toEvent(it.payload!!, it.type!!) }
+            .toList()
+        )
     }
 
     override fun query(vararg types: String): EventStream {
-        return EventStream.empty()
+        return EventStream(dsl.selectFrom(EVENT_STORE)
+            .where(EVENT_STORE.TYPE.`in`(types.asList()))
+            .orderBy(EVENT_STORE.SEQUENCE_ID.asc())
+            .fetch()
+            .map { EventJsonMapper.toEvent(it.payload!!, it.type!!) }
+            .toList()
+        )
     }
 
     override fun getLastEventId(aggregateId: AggregateId): EventId? {
