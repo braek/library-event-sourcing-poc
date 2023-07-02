99c25279e3a916182113ab99363fea48f3beec9a
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-07-02 22:36:17 +0200

    WIP (work in progress)

--- src/application/domain/src/main/kotlin/be/koder/library/domain/event/EventStreamChangedException.kt
+++ src/application/domain/src/main/kotlin/be/koder/library/domain/event/EventStreamChangedException.kt
@@ -2,7 +2,5 @@ package be.koder.library.domain.event
 
 import be.koder.library.vocabulary.event.EventId
 
-class EventStreamChangedException : RuntimeException {
-
-    constructor(lastEventId: EventId) : super(String.format("Cannot store events: event stream changed after event with ID %s", lastEventId))
-}
+class EventStreamChangedException(lastEventId: EventId) :
+    RuntimeException(String.format("Cannot store events: event stream changed after event with ID %s", lastEventId))
